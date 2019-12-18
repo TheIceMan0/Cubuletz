ca1de9975b312cdd75562424180035e28ccf4f7e
TheIceMan0 <48101628+TheIceMan0@users.noreply.github.com>
2019-12-18 12:23:26 +0200

    v2.0
    started working at mobile platform

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,53 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Android;
 
-public class PlayerMovement : MonoBehaviour {
 
-	public Rigidbody rb;
+public class PlayerMovement : MonoBehaviour
+{
 
-	private float forwardForce = 1200;
-	private float sidewaysForce = 60;
+    public Rigidbody rb;
+
+    private float forwardForce = 1200;
+    private float sidewaysForce = 60;
     private float x;
-    
+    private float stg;
+    private float width;
+    private float height;
 
-    void FixedUpdate ()
-	{
+
+    private void Start()
+    {
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+    }
+
+    void FixedUpdate()
+    {
+
         x = Input.GetAxis("Horizontal");
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        rb.AddForce(sidewaysForce * Time.deltaTime*x*2, 0, 0, ForceMode.VelocityChange);
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Move the cube if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos.x = (pos.x - width) / width;
+                rb.AddForce(sidewaysForce * Time.deltaTime * pos.x *1.5f, 0, 0, ForceMode.VelocityChange);
+
+            }
+
+        }
+        else
+            rb.AddForce(sidewaysForce * Time.deltaTime * x * 2, 0, 0, ForceMode.VelocityChange);
         if (rb.position.y < -1f)
-		{
-			FindObjectOfType<GameManager>().EndGame();
-		}
-	}
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        Debug.Log(x.ToString() + " " + stg.ToString());
+    }
 }
 
